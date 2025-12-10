86979e51446520b89c0b74f791cd07f0482c83f1
kara-py <ali16kara@gmail.com>
2025-12-10 13:53:17 +0100

    feat(HC-8): Add patient form toggle functionality in app.js

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const showAddPatientForm = document.getElementById("showAddPatientForm");
+    const addPatientForm = document.getElementById("addPatientForm");
+    
+    
+    showAddPatientForm.addEventListener("click", () =>{
+        console.log("showAddPatientForm test");
+        addPatientForm.classList.toggle("hidden");
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})
