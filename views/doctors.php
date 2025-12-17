26e6827b45e8e1319cd5c3ce2b0ac493b5bdc0a9
kara-py <ali16kara@gmail.com>
2025-12-17 18:50:35 +0100

    fix: Update dashboard link and remove extra whitespace in navigation

--- views/doctors.php
+++ views/doctors.php
@@ -167,7 +167,7 @@ if(isset($_POST['addPatient'])){
         </div>
 
         <nav class="mt-6">
-            <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-700 transition">
+            <a href="dashboard.php" class="flex items-center px-6 py-3 hover:bg-gray-700 transition">
                 <span>Dashboard</span>
             </a>
             <a href="patients.php" class="flex items-center px-6 py-3 hover:bg-gray-700 transition">
